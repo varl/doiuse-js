00ae26ad4926c23ba653e57e22da92606383062b
Viktor Varland <viktor@vardevs.se>
2019-02-04 07:03:04 +0100

    chore: commit intl/proxy/reflect

---
+++ features/es6/proxy.js
@@ -0,0 +1,21 @@
+const { types } = require('recast')
+
+exports.name = 'es6.proxy'
+
+exports.def = function (ast) {
+    let result = false
+
+    types.visit(ast, {
+        visitIdentifier: function (path) {
+            const node = path.node
+
+            if (node.name === 'Proxy') {
+                result = true
+            }
+
+            this.traverse(path)
+        },
+    })
+
+    return result
+}
---
+++ features/es6/reflect.js
@@ -0,0 +1,21 @@
+const { types } = require('recast')
+
+exports.name = 'es6.reflect'
+
+exports.def = function (ast) {
+    let result = false
+
+    types.visit(ast, {
+        visitIdentifier: function (path) {
+            const node = path.node
+
+            if (node.name === 'Reflect') {
+                result = true
+            }
+
+            this.traverse(path)
+        },
+    })
+
+    return result
+}
